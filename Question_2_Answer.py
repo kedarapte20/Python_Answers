ffde00f89531c05c9a3f4b5f104ba286b0fdc698
Kedar Apte <58838400+kedarapte20@users.noreply.github.com>
2021-08-16 16:50:07 +0530

    Update Question_2_Answer.py

--- Question_2_Answer.py
+++ Question_2_Answer.py
@@ -1,1 +1,7 @@
-
+import requests
+import json
+json_data = []
+for i in range(1,13):
+    response = requests.get(f"https://reqres.in/api/users?page={i}")
+    json_data.append(json.loads(response.text))
+print(len(json_data))
